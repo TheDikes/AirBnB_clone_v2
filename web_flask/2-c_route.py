4ca99e6629bbb923fcdac6bb3ea0b140bd218892
Maa-mmy <dikecynthia22@gmail.com>
2023-05-20 21:48:07 +0100

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,6 +12,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB"
@@ -26,5 +27,5 @@ def display_text(text):
     text = text.replace('_', ' ')
     return "C {}".format(text)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
