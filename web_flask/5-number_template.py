5658fdc04c99d155950d20749b93b2c8b0f0f263
Maa-mmy <dikecynthia22@gmail.com>
2023-05-21 00:45:29 +0100

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,9 +38,9 @@ def c(text):
     return "C {}".format(text)
 
 
-@app.route("/python", strict_slashes=False)
+@app.route("/python", defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
+def python(text):
     """Displays 'Python' followed by the value of <text>
 
     Replaces any underscores in <text> with slashes.
