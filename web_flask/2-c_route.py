9401167873a8857a65d9a3bdd9ab91f2615e5428
Maa-mmy <dikecynthia22@gmail.com>
2023-05-21 01:28:58 +0100

    C is fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: display "Hello HBNB!"
-    /hbnb: display "HBNB"
-    /c/<text>: display "C" followed by `text` variable
-    replacing underscore `_` symbols with a space` `)
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
-
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB"
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """Displays 'HBNB'."""
     return "HBNB"
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
 
-    """Replace underscores with spaces"""
-    text = text.replace('_', ' ')
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
     return "C {}".format(text)
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0")
